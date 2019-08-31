b86d9e75db3da6d9fc7097b67a525c080fe920ed
emmanuelmuchiri <emmanuel.muchiri@outlook.com>
2019-08-31 10:34:47 +0300

    created Profile model and defined it's columns

--- instagram/insta/models.py
+++ instagram/insta/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    Name = models.TextField(default="Anonymous")
+    profile_picture = models.ImageField(upload_to='users/', default='users/user.png')
+    bio = models.TextField(default="Welcome Me!")
