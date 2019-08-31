c01283885b5187550db542680f24b636fbd5af7e
emmanuelmuchiri <emmanuel.muchiri@outlook.com>
2019-08-31 10:40:24 +0300

    created instagram db and linked it with the application

--- instagram/instagram/settings.py
+++ instagram/instagram/settings.py
@@ -78,8 +78,10 @@ WSGI_APPLICATION = 'instagram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'instagram',
+        'USER': 'mannuh',
+        'PASSWORD':'Emmah@7127',
     }
 }
 
