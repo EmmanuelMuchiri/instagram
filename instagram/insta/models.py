3142becb68a51f64b658ca4ace3c9606d67956f9
emmanuelmuchiri <emmanuel.muchiri@outlook.com>
2019-08-31 12:46:57 +0300

    created the models to be used

--- instagram/insta/models.py
+++ instagram/insta/models.py
@@ -1,16 +1,31 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Profile(models.Model):
-    # user = models.OneToOneField(User, on_delete=models.CASCADE, default='profile')
-    profile_photo = models.ImageField(upload_to='users/', default='users/user.png')
-    bio = models.TextField(default="Instagram IG!")
+    user = models.ForeignKey(User,on_delete=models.CASCADE,default='user')
+    profile_name = models.TextField(default="Anonymous")
+    profile_picture = models.ImageField(upload_to='users/', default='users.jpg')
+    bio = models.TextField(default="Hey dear!")
+
+
+class Image(models.Model):
+    image_url = models.ImageField(upload_to = 'images/',blank=True,default="image_url")
+    image_name = models.CharField(max_length=30,default="title")
+    image_caption =  models.CharField(max_length=30,default="title")
+    profile = models.ForeignKey(Profile,on_delete=models.CASCADE,default='profile')
+    # comments = models.ManyToManyField(Comment)
+    # likes = models.ManyToManyField(Like)
+    
 
 class Comment(models.Model):
     text = models.TextField()
-    user = models.ForeignKey(Profile, default='comments', on_delete=models.CASCADE)
+    comment = models.ForeignKey(Image, default='comments',on_delete=models.CASCADE)
+    user = models.ForeignKey(Profile, default='comments',on_delete=models.CASCADE)
 
 
-class Likes(models.Model):
-    user = models.ForeignKey(Profile, default='mylikes', on_delete=models.CASCADE)
+class Like(models.Model):
+    user = models.ForeignKey(Profile, default='profile',on_delete=models.CASCADE)
+    image = models.ForeignKey(Image, default='likes',on_delete=models.CASCADE)
