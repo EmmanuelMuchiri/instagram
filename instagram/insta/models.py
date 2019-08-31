2cc84b99756068df3de3eab62e78e2c278383f95
emmanuelmuchiri <emmanuel.muchiri@outlook.com>
2019-08-31 11:00:30 +0300

    created table likes,defined the columns andd the relationships between profile and post

--- instagram/insta/models.py
+++ instagram/insta/models.py
@@ -4,20 +4,12 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-    Name = models.TextField(default="Anonymous")
     profile_picture = models.ImageField(upload_to='users/', default='users/user.png')
     bio = models.TextField(default="Welcome Me!")
 
-class Post(models.Model):
-    image = models.ImageField(upload_to='posts/')
-    user = models.ForeignKey(Profile, related_name='posts',on_delete=models.CASCADE)
-
 class Comment(models.Model):
     text = models.TextField()
-    photo = models.ForeignKey(Post, related_name='comments',on_delete=models.CASCADE)
     user = models.ForeignKey(Profile, related_name='comments',on_delete=models.CASCADE)
 
 class Likes(models.Model):
     user = models.ForeignKey(Profile, related_name='mylikes',on_delete=models.CASCADE)
-    photo = models.ForeignKey(Post, related_name='photolikes',on_delete=models.CASCADE)
