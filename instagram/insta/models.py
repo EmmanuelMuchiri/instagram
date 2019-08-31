3b54242fa16b9f4910eaa7ba3aa32c8b4f5d50d6
emmanuelmuchiri <emmanuel.muchiri@outlook.com>
2019-08-31 13:04:11 +0300

    defined the relationships between the profile,image,likes and comments

--- instagram/insta/models.py
+++ instagram/insta/models.py
@@ -1,31 +1,32 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
 
 class Profile(models.Model):
-    user = models.ForeignKey(User,on_delete=models.CASCADE,default='user')
-    profile_name = models.TextField(default="Anonymous")
-    profile_picture = models.ImageField(upload_to='users/', default='users.jpg')
-    bio = models.TextField(default="Hey dear!")
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    Name = models.TextField(default="Anonymous")
+    profile_picture = models.ImageField(upload_to='users/', default='users/user.png')
+    bio = models.TextField(default="Instagram!")
 
+class Comment(models.Model):
+    text = models.TextField()
+
+class Likes(models.Model):
+    text = models.TextField()
 
 class Image(models.Model):
     image_url = models.ImageField(upload_to = 'images/',blank=True,default="image_url")
     image_name = models.CharField(max_length=30,default="title")
-    image_caption =  models.CharField(max_length=30,default="title")
-    profile = models.ForeignKey(Profile,on_delete=models.CASCADE,default='profile')
-    # comments = models.ManyToManyField(Comment)
-    # likes = models.ManyToManyField(Like)
+    image_caption = models.TextField()
+    likes = models.ManyToManyField(Likes)
+    comments = models.ManyToManyField(Comment)
     
 
-class Comment(models.Model):
-    text = models.TextField()
-    comment = models.ForeignKey(Image, default='comments',on_delete=models.CASCADE)
-    user = models.ForeignKey(Profile, default='comments',on_delete=models.CASCADE)
+    def __str__(self):
+        return self.title
 
+    def save_image(self):
+        self.save()
 
-class Like(models.Model):
-    user = models.ForeignKey(Profile, default='profile',on_delete=models.CASCADE)
-    image = models.ForeignKey(Image, default='likes',on_delete=models.CASCADE)
+    def delete_image(self):
+        self.delete()
