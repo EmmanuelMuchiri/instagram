55d99abd6e1671df36e3f88c082c66570ae67b70
emmanuelmuchiri <emmanuel.muchiri@outlook.com>
2019-08-31 11:57:17 +0300

    added welcome email text and html

--- instagram/insta/models.py
+++ instagram/insta/models.py
@@ -1,15 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
 
 class Profile(models.Model):
-    profile_picture = models.ImageField(upload_to='users/', default='users/user.png')
-    bio = models.TextField(default="Welcome Me!")
+    # user = models.OneToOneField(User, on_delete=models.CASCADE, default='profile')
+    profile_photo = models.ImageField(upload_to='users/', default='users/user.png')
+    bio = models.TextField(default="Instagram IG!")
 
 class Comment(models.Model):
     text = models.TextField()
-    user = models.ForeignKey(Profile, related_name='comments',on_delete=models.CASCADE)
+    user = models.ForeignKey(Profile, default='comments', on_delete=models.CASCADE)
+
 
 class Likes(models.Model):
-    user = models.ForeignKey(Profile, related_name='mylikes',on_delete=models.CASCADE)
+    user = models.ForeignKey(Profile, default='mylikes', on_delete=models.CASCADE)
